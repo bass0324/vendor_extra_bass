74046432d85bf24320e0eda3107c457426310c09
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-29 09:54:06 -0700

    Add pixel theme

---
+++ product.mk
@@ -0,0 +1,11 @@
+PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+PRODUCT_PACKAGES += auditd
+
+$(call prepend-product-if-exists, vendor/bass0324/product.mk)
+
+# Themes
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+PRODUCT_PROPERTY_OVERRIDES += ro.boot.vendor.overlay.theme=com.google.android.theme.pixel
+PRODUCT_PROPERTY_OVERRIDES += ro.com.google.ime.theme_id=5
+PRODUCT_PACKAGES += \
+    PixelTheme
