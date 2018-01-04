16f766343dad9e2bf0f89db9d43b6aeff1b0e63c
John Basista <03bass23@gmail.com>
2018-01-03 19:57:37 -0500

    Revert "Update uri"
    This reverts commit 4b062d509de98c6caed9807600524ed9dd443d0c.

--- BoardConfigExtra.mk
+++ BoardConfigExtra.mk
@@ -1,3 +1,3 @@
 PRODUCT_DEFAULT_DEV_CERTIFICATE := /home/bass/.android-certs/releasekey
 PRODUCT_PROPERTY_OVERRIDES += \
-    cm.updater.uri=//192.168.1.38/
+    cm.updater.uri=smb://192.168.1.38/Update
